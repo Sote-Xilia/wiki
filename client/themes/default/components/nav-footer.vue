bd8af12045aae409979957d6b1f2edb1b9741614
SebastianMacaya <contacto@sebastianmacaya.com>
2021-10-18 10:35:51 -0300

    fix footer

--- client/themes/default/components/nav-footer.vue
+++ client/themes/default/components/nav-footer.vue
@@ -4,7 +4,7 @@
       template(v-if='company && company.length > 0 && contentLicense !== ``')
         span(v-if='contentLicense === `alr`') {{ $t('common:footer.copyright', { company: company, year: currentYear, interpolation: { escapeValue: false } }) }} |&nbsp;
         span(v-else) {{ $t('common:footer.license', { company: company, license: $t('common:license.' + contentLicense), interpolation: { escapeValue: false } }) }} |&nbsp;
-      span {{ $t('common:footer.poweredBy') }} #[a(href="#", ref='nofollow') Xilia]
+      span {{ $t('common:footer.poweredBy') }} #[a(href="https://js.wiki/", ref='nofollow') Wiki.js]
 </template>
 
 <script>
